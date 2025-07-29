c6798800676565234e253585d5e74fe1a2f0d7d5
Linqi1234 <sunlinqi110@gmail.com>
2025-07-29 17:52:56 +0800

    P2676

--- p2676.cpp
+++ p2676.cpp
@@ -1,6 +1,26 @@
+//1 <= N <= 2e4, 1 <= Hi <= 1e4, 1 <= B <= S <= 2e9 + 7;
+//O(n), O(n);
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
+const int MAXN = 20000;
+int h[MAXN];
+
 int main() {
+    int n, b;
+    cin >> n >> b;
+    for (int i = 0; i < n; i++) 
+        cin >> h[i];
+
+    sort(h, h + n, greater<int>()); 
+
+    int sum = 0, cnt = 0;
+    for (int i = 0; i < n; i++) {
+        sum += h[i];
+        cnt++;
+        if (sum >= b) break; 
+    }
+    cout << cnt << endl;
     return 0;
 }
